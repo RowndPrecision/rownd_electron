fa2f4580c8941095ad78abb39680683f728f12b8
yakupad <yakup@rowndprecision.com>
2024-06-23 13:21:55 +0300

    Package name changes

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,11 +1,11 @@
 # #!/bin/bash
 
-sudo setcap cap_net_admin,cap_net_raw+eip /opt/CNCjs/cncjs-app
-sudo chmod 4755 /opt/CNCjs/chrome-sandbox
-# sudo chown root:root /opt/CNCjs/chrome-sandbox
+sudo setcap cap_net_admin,cap_net_raw+eip /opt/Rownd/rownd-app
+sudo chmod 4755 /opt/Rownd/chrome-sandbox
+# sudo chown root:root /opt/Rownd/chrome-sandbox
 
-echo "/opt/CNCjs" | sudo tee /etc/ld.so.conf.d/electron.conf
+echo "/opt/Rownd" | sudo tee /etc/ld.so.conf.d/electron.conf
 sudo ldconfig
 
-sudo mkdir -p /opt/CNCjs/resources/app/server/services/phonebleconnection/files
-sudo chown -R rownd /opt/CNCjs/resources/app/server/services/phonebleconnection/files
+sudo mkdir -p /opt/Rownd/resources/app/server/services/phonebleconnection/files
+sudo chown -R rownd /opt/Rownd/resources/app/server/services/phonebleconnection/files
